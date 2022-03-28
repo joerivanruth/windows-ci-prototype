8a4eaf5b624e27b643128b3ea37428fe269653a3
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2022-03-28 16:12:26 +0200

    Set the env

--- test.py
+++ test.py
@@ -5,11 +5,12 @@ import pymonetdb
 
 db_name = environ.get('DB_NAME', 'demo')
 db_host = environ.get('DB_HOST', 'localhost')
+db_port = environ.get('DB_PORT', 50000)
 
 print("Hello, world!")
-print(f"Connecting to «{db_name}» on «{db_host}»")
+print(f"Connecting to «{db_name}» on «{db_host}:{db_port}»")
 
-conn = pymonetdb.connect('demo', host='localhost')
+conn = pymonetdb.connect('demo', host='localhost', port=db_port)
 cursor = conn.cursor()
 
 cursor.execute("SELECT * FROM environment")
